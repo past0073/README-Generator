877b5ebe866fd224beb5f5cac89e3f4299f1c571
AP <past0073@umn.edu>
2021-02-13 09:29:28 -0600

    Added required prompts.

--- index.js
+++ index.js
@@ -1,3 +1,62 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'discription',
+        message: 'Provide a discrption of your project: '
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Provide installation instructions: '
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide usage information: '
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'Provide contribution guidelines: '
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide test instructions: '
+    },
+    {
+        type: 'list',
+        name: 'licenses',
+        message: 'Which license would you like to use?',
+        choices: ['MIT', 'GNU GPLv3', 'Apache', 'Mozilla', 'GNU AGPLv3']
+    },
+    {
+        type: 'input',
+        name: 'githubUser',
+        message: 'Provide your gitHub username: '
+    },
+    {
+        type: 'input',
+        name: 'githubLink',
+        message: 'Provide your gitHub profile URL: '
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Provide your email address: '
+    },
+
+]).then((response) => {
+    fs.writeFile('info.txt', JSON.stringify(response), (err) =>
+    err ? console.log(err) : console.log("File written! :D")
+    )
+
+});
